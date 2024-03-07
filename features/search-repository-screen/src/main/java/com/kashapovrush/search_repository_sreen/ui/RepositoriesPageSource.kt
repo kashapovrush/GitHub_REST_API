9f901b5d7715bb13dffdb3a3b95eaef17f29cf76
rushankasapov <kashapovrush@mail.ru>
2024-03-07 06:43:08 +0300

    Initial commit

--- features/search-repository-screen/src/main/java/com/kashapovrush/search_repository_sreen/ui/RepositoriesPageSource.kt
+++ features/search-repository-screen/src/main/java/com/kashapovrush/search_repository_sreen/ui/RepositoriesPageSource.kt
@@ -8,26 +8,26 @@ import javax.inject.Inject
 
 class RepositoriesPageSource @Inject constructor(
     private val apiService: ApiService
-) : PagingSource<Int, SearchItem>() {
-    override fun getRefreshKey(state: PagingState<Int, SearchItem>): Int? {
-//        return state.anchorPosition
-        TODO()
-    }
-
-    override suspend fun load(params: LoadParams<Int>): LoadResult<Int, SearchItem> {
-//        return try {
-//            val start = params.key ?: 1
-//            val limit = 20
-//            val response =
-//                apiService.searchRepositories("kotlin", start, limit) ?: emptyList<SearchItem>()
-//            val nextKey = if (response.isEmpty()) null else response.size.plus(start).plus(1)
-//            val prevKey = if (start == 1) null else response.size.minus(limit)
-//            LoadResult.Page(
-//                response, nextKey, prevKey
-//            )
-//        } catch (e: Exception) {
-//            return LoadResult.Error(e)
-//        }
-        TODO()
-    }
+) {
+//    override fun getRefreshKey(state: PagingState<Int, SearchItem>): Int? {
+////        return state.anchorPosition
+//        return null
+//    }
+//
+//    override suspend fun load(params: LoadParams<Int>): LoadResult<Int, SearchItem> {
+////        return try {
+////            val start = params.key ?: 1
+////            val limit = 20
+////            val response =
+////                apiService.searchRepositories("kotlin", start, limit) ?: emptyList<SearchItem>()
+////            val nextKey = if (response.isEmpty()) null else response.size.plus(start).plus(1)
+////            val prevKey = if (start == 1) null else response.size.minus(limit)
+////            LoadResult.Page(
+////                response, nextKey, prevKey
+////            )
+////        } catch (e: Exception) {
+////            return LoadResult.Error(e)
+////        }
+//        return null
+//    }
 }
